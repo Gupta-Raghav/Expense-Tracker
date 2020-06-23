456b0d1c73a368480ea89fb86e4b0fa4f1c8c874
raghuweer23 <raghuweer23@gmail.com>
2020-06-23 22:07:51 +0530

    Setup dev dependencies and dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ const CSSExtract = new MiniCssExtractPlugin('styles.css');
     return {
         entry: './src/app.js',
         output: {
-            path: path.join(__dirname,'public'),
+            path: path.join(__dirname,'public','dist'),
             filename: 'bundle.js'
         },
         module: {
@@ -45,7 +45,8 @@ const CSSExtract = new MiniCssExtractPlugin('styles.css');
         devtool: isProduction? 'source-map':'inline-source-map',
         devServer: {
             contentBase: path.join(__dirname, 'public'),
-            historyApiFallback: true
+            historyApiFallback: true,
+            publicPath: '/dist/'
         }
     };
 }
