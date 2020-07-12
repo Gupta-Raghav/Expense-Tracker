a7ebd582799a6abba0d174733b88bb5063b3e4eb
raghuweer23 <raghuweer23@gmail.com>
2020-07-12 13:49:26 +0530

    Firebase Database URL fixes

--- webpack.config.js
+++ webpack.config.js
@@ -52,7 +52,7 @@ const CSSExtract = new MiniCssExtractPlugin('styles.css');
             new webpack.DefinePlugin({
                 'process.env.FIREBASE_API_KEY' : JSON.stringify(process.env.FIREBASE_API_KEY),
                 'process.env.FIREBASE_AUTH_DOMAIN' : JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN ),
-                'process.env.FIREBASE_DATABSE_URL' : JSON.stringify(process.env.FIREBASEDATABSE_URL),
+                'process.env.FIREBASE_DATABSE_URL' : "'https://expensify-8fb86.firebaseio.com'",
                 'process.env.FIREBASE_PROJECT_ID' : JSON.stringify(process.env.FIREBASE_PROJECT_ID),
                 'process.env.FIREBASE_STORAGE_BUCKET' : JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
                 'process.env.FIREBASE_APP_ID' : JSON.stringify(process.env.FIREBASE_APP_ID),
